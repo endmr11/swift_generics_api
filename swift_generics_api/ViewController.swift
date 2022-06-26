d0652896fd5c2a8ec4c9bdcfa6dd4a6adf7d02b0
Eren Demir <73402961+endmr11@users.noreply.github.com>
2022-06-27 00:22:24 +0300

    Update ViewController.swift

--- swift_generics_api/ViewController.swift
+++ swift_generics_api/ViewController.swift
@@ -6,7 +6,7 @@ class ViewController: UIViewController {
 
     let usersUrl = URL(string: "https://jsonplaceholder.typicode.com/users")
     let todolistUrl = URL(string: "https://jsonplaceholder.typicode.com/todos")
-    
+    let kisilerUrl = URL(string: "")
     private var models:[Codable] = []
     
     private let tableView: UITableView = {
@@ -22,9 +22,13 @@ class ViewController: UIViewController {
         tableView.dataSource = self
         //fetchUsersData()
         //fetchTodosData()
+        Task.init{
+            await addTodosDataAsync()
+        }
         Task.init {
             await fetchTodosDataAsync()
         }
+
     }
     
     override func viewDidLayoutSubviews() {
@@ -70,6 +74,7 @@ class ViewController: UIViewController {
         let result = try? await URLSession.shared.customRequestAsync(
             url: todolistUrl,
             model: [TodoModel].self,
+            requestType: RequestType.GET,
             queryParameters: nil,
             body: nil,
             onFail: {
@@ -84,6 +89,26 @@ class ViewController: UIViewController {
             }
         case .failure(let error):
             print(error)
+        case .none:
+            print("none")
+        }
+    }
+    
+    func addTodosDataAsync() async {
+        let result = try? await URLSession.shared.customRequestAsync(
+            url: kisilerUrl,
+            model: Kisi.self,
+            requestType: RequestType.POST,
+            queryParameters: nil,
+            body: ["kisi_ad":"TestAlo","kisi_tel":"5454"],
+            onFail: {
+                print("onFail")
+            })
+        switch result{
+        case .success(let todos):
+            print(">>>>>> success: \(todos)")
+        case .failure(let error):
+            print(">>>>>> error: \(error)")
         case .none:
             print("none")
         }
@@ -149,19 +174,53 @@ extension URLSession{
     func customRequestAsync<T: Codable>(
         url: URL?,
         model:T.Type,
+        requestType: RequestType,
         queryParameters: [String: String]?,
-        body: [String: String]?,
+        body: [String: Any]?,
         onFail: @escaping () -> ()
     )async throws -> Result<T,Error> {
-        guard let url = url else {
-            return (.failure(NetworkError.urlError))
-        }
-        let (data,_) = try await URLSession.shared.data(from: url)
-
-        if let datas = try? JSONDecoder().decode(model, from: data){
-            return .success(datas)
+        let res = try? await handleRequest(url: url,model: model, requestType: requestType, queryParameters: queryParameters, body: body, onFail: onFail)
+        if let res = res {
+            return res
         }
         return .failure(NetworkError.dataError)
+    }
+    
+    func handleRequest<T: Codable>(
+        url: URL?,
+        model:T.Type,
+        requestType: RequestType,
+        queryParameters: [String: String]?,
+        body: [String: Any]?,
+        onFail: @escaping () -> ()
+    ) async throws -> Result<T,Error> {
+        if requestType == .GET {
+            guard let url = url else {
+                return (.failure(NetworkError.urlError))
+            }
+            let (data,_) = try await URLSession.shared.data(from: url)
+
+            if let datas = try? JSONDecoder().decode(model, from: data){
+                return .success(datas)
+            }
+            return .failure(NetworkError.dataError)
+        }else if requestType == .POST {
+                guard let url = url else {
+                    return (.failure(NetworkError.urlError))
+                }
+                var request = URLRequest(url: url)
+            request.httpMethod = requestType.rawValue
+                request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                let payload = try? JSONSerialization.data(withJSONObject: body!, options: [])
+                let (data, _) = try await URLSession.shared.upload(for: request, from: payload!)
+                if let datas = try? JSONDecoder().decode(model, from: data){
+                    return .success(datas)
+                }
+                return .failure(NetworkError.urlError)
+        }else{
+            return .failure(NetworkError.urlError)
+        }
+        
     }
 }
 struct UserModel:Codable {
@@ -172,4 +231,25 @@ struct UserModel:Codable {
 struct TodoModel:Codable {
     let title: String
     let completed: Bool
+}
+
+
+class Kisi:Codable {
+    var kisi_id:String?
+    var kisi_ad:String?
+    var kisi_tel:String?
+    
+    
+    init(kisi_id:String,kisi_ad:String,kisi_tel:String)  {
+        self.kisi_id = kisi_id
+        self.kisi_ad = kisi_ad
+        self.kisi_tel = kisi_tel
+    }
+}
+
+enum RequestType: String, CaseIterable {
+   case GET
+   case PUT
+   case DELETE
+   case POST
 }
